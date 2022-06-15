690946b9a591b1d0e9a473e809c4f97d4e234614
Fabian Villasenor <exionite@gmail.com>
2022-06-15 13:19:07 -0700

    neon glow

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -12,9 +12,10 @@ const textVariant = {
   },
   neonBox: {
     boxShadow:  "0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #bc13fe, 0 0 0.8rem #bc13fe, 0 0 2.8rem #bc13fe, inset 0 0 1.3rem #bc13fe",
+    textShadow: "0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 42px #bc13fe, 0 0 82px #bc13fe, 0 0 92px #bc13fe, 0 0 102px #bc13fe, 0 0 151px #bc13fe",
     transition: {
       duration: 1,
-      yoyo: Infinity
+      yoyo: Infinity,
     }
   }
 }
