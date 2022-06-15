01c4376ddefacd51094d84135822ee24b2dcb8cb
Fabian Villasenor <exionite@gmail.com>
2022-06-15 15:34:31 -0700

    projects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Body from './Components/Body/Body';
 import Navbar from './Components/Navbar/Navbar';
+import ProjectDisplay from './Components/ProjectDisplay/ProjectDisplay';
+import ProjectsComp from './Components/ProjectsComp/ProjectsComp';
 import StainedGlass from './Components/StainedGlass/StainedGlass';
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <div className="App">
       {/* <Navbar /> */}
       <Body />
+      <ProjectDisplay />
       {/* <StainedGlass /> */}
     </div>
   );
---
+++ src/Components/ProjectDisplay/ProjectDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ProjectDisplay.scss"
+
+export default function ProjectDisplay() {
+  return (
+    <div className='projectDisplay-container'>
+      <div className="image-container">
+        <p>test</p>
+      </div>
+    </div>
+  )
+}
