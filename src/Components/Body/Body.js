2d32ce326c82c87967948368c93b796884f2535a
Fabian Villasenor <exionite@gmail.com>
2022-06-15 09:52:17 -0700

    neon animation

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -5,7 +5,7 @@ export default function Body() {
   return (
     <div className='body-container'>
       <div className="name-container">
-        <h1 className='neon-text'>Fabian Villasenor</h1>
+        <h1 className='neon-text'>Fabian Villaseñor</h1>
       </div>
     </div>
   )
