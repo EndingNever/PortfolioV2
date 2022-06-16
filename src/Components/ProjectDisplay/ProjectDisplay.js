90d0515e31b0386bea168f5935d107b05012d026
Fabian Villasenor <exionite@gmail.com>
2022-06-16 12:26:09 -0700

    projects

--- src/Components/ProjectDisplay/ProjectDisplay.js
+++ src/Components/ProjectDisplay/ProjectDisplay.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import "./ProjectDisplay.scss"
-import noomPreview from "../../noomPreview.gif"
+import noomPreview from "../../eznoompreview.gif"
+import Navbar from '../Navbar/Navbar'
 
 export default function ProjectDisplay() {
   return (
     <div className='projectDisplay-container'>
       <div className="project-container">
         <img src={noomPreview} alt="" />
+      </div>
+      <div className="select-projects">
+        <div className="project">1</div>
+        <div className="project">2</div>
+        <div className="project">3</div>
       </div>
     </div>
   )
