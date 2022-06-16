c232e90a39e9abcafc10d6ff0d1f970e2184d91a
Fabian Villasenor <exionite@gmail.com>
2022-06-16 14:46:53 -0700

    neon projects text

--- src/Styled.js
+++ src/Styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const ProjectsHeader = styled.h1`
 font-family: "Yellowtail", sans-serif;
-border: 0.2rem solid #bc13fe;
+/* border: 0.2rem solid #bc13fe; */
 cursor: pointer; //TODO Make this element an anchor tag that jumps to the projects
 position: absolute;
 font-size: 34px;
@@ -11,7 +11,11 @@ margin-left: 24px;
 transform: rotate(-40deg);
 display: inline-block;
 padding: 1em;
-    box-shadow:  0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #bc13fe, 0 0 0.8rem #bc13fe, 0 0 2.8rem #bc13fe, inset 0 0 1.3rem #bc13fe;
-    text-shadow:  0 0 7px #0fa, 0 0 10px #0fa, 0 0 21px #0fa,   0 0 42px #0fa,     0 0 82px #0fa,     0 0 92px #0fa,     0 0 102px #0fa,     0 0 151px #0fa;
-    /* text-shadow: 0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 42px #bc13fe, 0 0 82px #bc13fe, 0 0 92px #bc13fe, 0 0 102px #bc13fe, 0 0 151px #bc13fe; */
+/*Green Border, Pink Text */
+box-shadow:  0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #0fa, 0 0 0.8rem #0fa, 0 0 2.8rem #0fa, inset 0 0 1.3rem #0fa;
+    text-shadow:  0 0 7px #bc13fe, 0 0 10px #bc13fe, 0 0 21px #bc13fe,   0 0 42px #bc13fe,     0 0 82px #bc13fe,     0 0 92px #bc13fe,     0 0 102px #bc13fe,     0 0 151px #bc13fe;
+
+    /* Pink Text, Green Border */
+     /* box-shadow:  0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #0fa, 0 0 0.8rem #0fa, 0 0 2.8rem #0fa, inset 0 0 1.3rem #0fa;
+    text-shadow:  0 0 7px #bc13fe, 0 0 10px #bc13fe, 0 0 21px #bc13fe,   0 0 42px #bc13fe,     0 0 82px #bc13fe,     0 0 92px #bc13fe,     0 0 102px #bc13fe,     0 0 151px #bc13fe;  */
 `
