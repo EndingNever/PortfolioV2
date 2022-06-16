8d52008ab575e5e5507c695677e4ad061d550d34
Fabian Villasenor <exionite@gmail.com>
2022-06-16 12:51:07 -0700

    projects

--- src/Components/ProjectDisplay/ProjectDisplay.js
+++ src/Components/ProjectDisplay/ProjectDisplay.js
@@ -3,6 +3,30 @@ import "./ProjectDisplay.scss"
 import noomPreview from "../../eznoompreview.gif"
 import Navbar from '../Navbar/Navbar'
 
+const projectsForMapping = [
+  {
+    title: "Noom Cafe",
+    github: "https://github.com/EndingNever/NoomCafe",
+    siteLink: "https://endingnever.github.io/NoomCafe/",
+    screenshot: "",
+    heroPreview: ""
+  },
+  {
+    title: "Elden Ring",
+    github: "https://github.com/EndingNever/EldenRing",
+    siteLink: "https://endingnever.github.io/EldenRing/",
+    screenshot: "",
+    heroPreview: ""
+  },
+  {
+    title: "Data Science Portfolio",
+    github: "https://github.com/EndingNever/FonsecaWebsite",
+    siteLink: "https://devinfonseca.com/",
+    screenshot: "",
+    heroPreview: ""
+  }
+]
+
 export default function ProjectDisplay() {
   return (
     <div className='projectDisplay-container'>
@@ -10,9 +34,16 @@ export default function ProjectDisplay() {
         <img src={noomPreview} alt="" />
       </div>
       <div className="select-projects">
-        <div className="project">1</div>
+        {projectsForMapping.map((project) => (
+          <div className="project">
+            <h1>{project.title}</h1>
+            <a>{project.siteLink}</a>
+            <a>{project.github}</a>
+          </div>
+        ))}
+        {/* <div className="project">1</div>
         <div className="project">2</div>
-        <div className="project">3</div>
+        <div className="project">3</div> */}
       </div>
     </div>
   )
