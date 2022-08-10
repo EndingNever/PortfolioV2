de33b2f22acd1f4a4662779fde02900e4a169b23
exionite <exionite@gmail.com>
2022-08-10 14:57:35 -0700

    links to github, linkedin

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Body.scss"
 import { motion } from 'framer-motion'
 import Navbar from '../Navbar/Navbar'
+import { ProjectsHeader } from '../../Styled'
 
 const textVariant = {
   neon: {
@@ -13,7 +14,7 @@ const textVariant = {
     }
   },
   neonBox: {
-    boxShadow:  "0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #bc13fe, 0 0 0.8rem #bc13fe, 0 0 2.8rem #bc13fe, inset 0 0 1.3rem #bc13fe",
+    boxShadow: "0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #bc13fe, 0 0 0.8rem #bc13fe, 0 0 2.8rem #bc13fe, inset 0 0 1.3rem #bc13fe",
     textShadow: "0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 42px #bc13fe, 0 0 82px #bc13fe, 0 0 92px #bc13fe, 0 0 102px #bc13fe, 0 0 151px #bc13fe",
     transition: {
       duration: 1,
@@ -28,6 +29,12 @@ export default function Body() {
     <div className='body-container'>
       <div className="name-container">
         <motion.h1 variants={textVariant} animate="neonBox" className='neon-text'>Fabian Villaseñor</motion.h1>
+      </div>
+      <div className="social-media">
+        <a href="https://www.linkedin.com/in/fabianvillasenor/" target="_blank">
+          <ProjectsHeader className="linkedin">LinkedIn</ProjectsHeader>
+        </a>
+        <ProjectsHeader className="github">Github</ProjectsHeader>
       </div>
     </div>
   )
