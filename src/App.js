94d3e58844559fcc09757ebd18f2fe7c294cfa8e
exionite <exionite@gmail.com>
2022-08-11 11:40:08 -0700

    rm old files

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import Body from './Components/Body/Body';
 import Contact from './Components/Contact/Contact';
 import Navbar from './Components/Navbar/Navbar';
 import ProjectDisplay from './Components/ProjectDisplay/ProjectDisplay';
-import ProjectsComp from './Components/ProjectsComp/ProjectsComp';
 import Resume from './Components/Resume/Resume';
-import StainedGlass from './Components/StainedGlass/StainedGlass';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
       <Body />
       <ProjectDisplay />
       <Resume />
       <Contact />
-      {/* <StainedGlass /> */}
     </div>
   );
 }
