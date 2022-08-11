60b1695a17cf5fb9f2e07d7420c6310951d5ebb5
exionite <exionite@gmail.com>
2022-08-11 15:25:43 -0700

    resume carousel

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ProjectsHeader } from '../../Styled'
 import "./Resume.scss"
 // import resume from "./ResumeFinal.pdf"
 // import resume from "./ResumeFinal.pdf"
 
 export default function Resume() {
+
+  const [image, setImage] = useState("/files/resume1.jpg")
+
+  const imageCarousel = () => {
+    if (image === "/files/resume1.jpg") {
+      setImage("/files/resume2.jpg");
+    }
+    else if (image === "/files/resume2.jpg") {
+      setImage("/files/resume1.jpg")
+    }
+  }
+
   return (
     <div className='resume-container'>
-      <ProjectsHeader>Resume</ProjectsHeader>
+      <ProjectsHeader>Resume </ProjectsHeader>
+      <ProjectsHeader className='download'>Download</ProjectsHeader>
       <div className="resume">
-        <iframe className='iframe' title="Resume" src={process.env.PUBLIC_URL + "/files/Resume3.pdf"} frameBorder="0">
-        </iframe>
-        {/* <embed src={process.env.PUBLIC_URL + "./ResumeFinal.pdf"} type="application/pdf" width="100%" height="100%">
-           <p>Your web browser doesn't have a PDF plugin.
-            Instead you can <a href={resume}>click here to
-              download the PDF file.</a></p> 
-        </embed> */}
+        <embed className='iframe' title="Resume" src={process.env.PUBLIC_URL + "/files/Resume3.pdf"} frameBorder="0">
+        </embed>
+        <div className="resume-image">
+          <img src={image} alt="resume" />
+          <div onClick={imageCarousel} className="angle">
+            <div className="left">
+              {"<"}
+            </div>
+            <div className="right">
+              {">"}
+            </div>
+          </div>
+        </div>
+        {/* <img src="/files/resume2.jpg" alt="" /> */}
       </div>
     </div>
   )
